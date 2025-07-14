b5d8aa6eec7ed58962fe65f00ca938fe3671130b
Lyp <2162686979@qq.com>
2025-07-14 17:28:56 +0800

    移除无用代码，添加注释，类型判断

--- src/app/equipmentCenter/components/EquipCard.tsx
+++ src/app/equipmentCenter/components/EquipCard.tsx
@@ -1,121 +1,6 @@
-//
-//
-//
-// // @ts-ignore
-// import React, {useState} from 'react';
-// import {Card, Space, Button, Modal} from 'antd';
-// import { useNavigate } from 'react-router-dom';
-// // @ts-ignore
-// import online from '../../../assets/online.png';
-// // @ts-ignore
-// import recall from '../../../assets/recall.png';
-// // @ts-ignore
-// import synchronous from '../../../assets/synchronous.png';
-// // @ts-ignore
-// import intask from '../../../assets/intask.png';
-// // @ts-ignore
-// import UAV from '../assets/UAV.png';
-// // @ts-ignore
-// import electricity from '../assets/electricity.png';
-// // @ts-ignore
-// import number from '../assets/number.png';
-// // @ts-ignore
-// import blueTeeth from '../assets/blueTeeth.png';
-//
-// const { Meta } = Card;
-//
-// const EquipCard: React.FC = () => {
-//     const navigate = useNavigate();
-//
-//     const handleTaskClick = () => {
-//         navigate('/task-center');
-//     };
-//     const [isModalVisible,setIsModalVisible] = useState(false)
-//     const showModal = ()=> {
-//         setIsModalVisible(true);
-//     };
-//     const handleOk = () => {
-//         console.log('设备已删除');
-//         setIsModalVisible(false);
-//     }
-//     const handleCancel = () => {
-//         setIsModalVisible(false);
-//     }
-//
-//     return (
-//         <Card
-//             hoverable
-//             style={{ width: 340 }}
-//             cover={
-//                 <img
-//                     alt="example"
-//                     src={UAV}
-//                     style={{
-//                         width: '250px',
-//                         height: '200px',
-//                         display: 'block',
-//                         margin: '0 auto',
-//                     }}
-//                 />
-//             }
-//             extra={
-//                 <Space>
-//                     <Button size="small">无人机01</Button>
-//                     <Button size="small" onClick={handleTaskClick}>
-//                         <img src={intask} width={14} style={{ marginRight: 4 }} />
-//                         任务中
-//                     </Button>
-//                     <Button size="small" style={{ marginRight: '20px' }}>
-//                         <img src={online} width={14} style={{ marginRight: 4 }} />
-//                         在线
-//                     </Button>
-//                 </Space>
-//             }
-//         >
-//             <Button size="small" style={{ marginTop: 10, marginRight: '20px' }}>
-//                 <img src={recall} width={14} style={{ marginRight: 4 }} />
-//                 召回
-//             </Button>
-//             <Button size="small" style={{ marginTop: 10, marginRight: '20px' }}>
-//                 <img src={synchronous} width={14} style={{ marginRight: 4 }} />
-//                 同步
-//             </Button>
-//             <Button size="small" style={{ marginTop: 10 }}>
-//                 巡检
-//             </Button>
-//             <div
-//                 className="cardFooter"
-//                 style={{
-//                     marginTop: '20px',
-//                     display: 'flex',
-//                     alignItems: 'center'
-//                 }}
-//             >
-//                 电量
-//                 <img src={electricity} width={20} style={{ marginRight: 4, marginLeft: '10px' }} />
-//                 <img src={number} width={40} style={{ marginRight: 4, marginLeft: '5px' }} />
-//                 <img src={blueTeeth} width={20} style={{ marginRight: 4, marginLeft: '10px' }} />
-//                 <Button type="primary" style={{ marginLeft: '20px' }} onClick={showModal}>删除设备</Button>
-//                 <Modal title="提示"
-//                        visible={isModalVisible}
-//                        onOk = {handleOk}
-//                        onCancel={handleCancel}
-//                        okText="删除"
-//                        cancelText="取消"
-//
-//                 >
-//                     <p>确认删除该设备？</p>
-//
-//                 </Modal>
-//
-//             </div>
-//         </Card>
-//     );
-// };
-//
-// export default EquipCard;
-//
-
+/**
+ * 设备中心-卡片组件
+ */
 
 // @ts-ignore
 import React, { useState, useEffect } from 'react';
@@ -222,11 +107,17 @@ const EquipCard: React.FC <{filters: FilterProps}> = ({filters}) => {
         // @ts-ignore
         // @ts-ignore
         // @ts-ignore
-        const batteryLevelMatch =
-            filters.batteryLevel === '1' ? equipment.battery_level_display.includes('80~100%') :
-                filters.batteryLevel === '2' ? equipment.battery_level_display.includes('50~80%') :
-                    filters.batteryLevel === '3' ? equipment.battery_level_display.includes('0~50%') :
-                        true;
+        let batteryLevelMatch: boolean;
+        if (filters.batteryLevel === '1') {
+            // @ts-ignore
+            batteryLevelMatch = equipment.battery_level_display.includes('80~100%');
+        } else {
+            // @ts-ignore
+
+            batteryLevelMatch = filters.batteryLevel === '2' ? equipment.battery_level_display.includes('50~80%') :
+                filters.batteryLevel === '3' ? equipment.battery_level_display.includes('0~50%') :
+                    true;
+        }
 
         return taskStatusMatch && connectionStatusMatch && batteryLevelMatch;
     });
