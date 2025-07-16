fa494b0d3e4d2baccdcc7558c3486865c8c4b3e0
Lyp <2162686979@qq.com>
2025-07-16 16:11:16 +0800

    新增-聊天页面

--- src/app/layout/components/DoChat.tsx
+++ src/app/layout/components/DoChat.tsx
@@ -1,14 +1,38 @@
 // @ts-ignore
 import React from 'react';
-import { ConfigProvider } from 'antd';
+import { Button, ConfigProvider, Input } from 'antd';
+import { ArrowUpOutlined } from "@ant-design/icons";
 
 const DoChatPage: React.FC = () => {
+    const handleSend = () => {
+        console.log('发送');
+    };
     return (
-
-        <div>
-            聊天页面
+        <div className="DoChatPage" style={{ position: 'relative', width: '1500px',marginTop :'700px',marginLeft :'50px'}}>
+            <Input.TextArea
+                rows={2}
+                placeholder="请输入你的问题..."
+                style={{
+                    paddingRight: '50px',
+                    borderRadius: '20px',
+                    height: '80px',
+                    width: '100%',
+                }}
+            />
+            <Button
+                type="primary"
+                icon={<ArrowUpOutlined />}
+                onClick={handleSend}
+                style={{
+                    position: 'absolute',
+                    right: '10px',
+                    bottom: '10px',
+                    borderRadius: '50%',
+                    width: '40px',
+                    height: '40px',
+                }}
+            />
         </div>
-
     );
 };
 
