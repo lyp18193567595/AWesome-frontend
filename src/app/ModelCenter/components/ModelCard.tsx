8f10f6a395b805169343edb45f16419b123e1ceb
Lyp <2162686979@qq.com>
2025-07-18 13:08:36 +0800

    技能模型广场-卡片调整

--- src/app/ModelCenter/components/ModelCard.tsx
+++ src/app/ModelCenter/components/ModelCard.tsx
@@ -3,19 +3,29 @@
  */
 // @ts-ignore
 import React from 'react';
-import { Card } from 'antd';
+import {Button, Card, Tag} from 'antd';
 
 const { Meta } = Card;
 // @ts-ignore
-import ModelImage from '../assets/ModelImage.png'
-const ModelCard: React.FC = () => (
-    <Card
-    hoverable
-    style={{ width: 240 }}
-    cover={<img alt="example" src={ModelImage} />}
->
-</Card>
+import ModelImage from '../assets/model.png'
+import Title from "antd/es/skeleton/Title";
 
+const ModelCard: React.FC = () => (
+
+    <Card
+        hoverable
+        style={{ width: 240 }}
+        cover={<img alt="example" src={ModelImage} />}
+    >
+        <Meta title="巡检专家" style={{marginBottom :'20px'}}></Meta>
+        <Meta title="模型描述" style={{marginBottom :'20px'}} ></Meta>
+        <Tag>标签</Tag>
+        <Tag>标签</Tag> <Tag>标签</Tag>
+
+        <Button type="primary" block style={{marginTop :'50px'}}>
+            使用
+        </Button>
+    </Card>
 );
 
 export default ModelCard;
